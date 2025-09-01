0697f5e41939b405446cc36a89b4509f9a1dc54f
Yash Priyadarshan <yashpriyadarshan@gmail.com>
2025-09-01 15:27:09 +0530

    Added MySqp database instead of temperory database

--- src/main/java/com/yash/taskmanager/dto/AddTaskDto.java
+++ src/main/java/com/yash/taskmanager/dto/AddTaskDto.java
@@ -1,31 +1,22 @@
 package com.yash.taskmanager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AddTaskDto {
 
     private String name;
     private String description;
+    private boolean completed;
 
     public AddTaskDto() {
     }
 
-    public AddTaskDto(String name, String description) {
+    public AddTaskDto(String name, String description, boolean completed) {
         this.name = name;
         this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+        this.completed = completed;
     }
 }
